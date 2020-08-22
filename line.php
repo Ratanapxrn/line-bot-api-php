d438263d42903d3b1e5cad8d6e3580313a8d3b6e
Ratanapxrn <70052100+Ratanapxrn@users.noreply.github.com>
2020-08-22 15:31:25 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'MI5/Zsb0N/oB/oBGw0V7hh8UhCvWMbCANwXkd+2mSHwPIxhnvww8EKjWzvfE7Q9OeivgZj8B32ojTXYKMUuptQB5EzSX89MiBnOzP/Yv9AVvjUxQgoQkeMeBjhJBG+tOPk+LxrGdaIqhzmtZJODiHQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'Y9WqQnQukC2Yh+bdGCqsYpa+IKOPX/W5oaK07W0VllBbps1XdnkdvlF2Hvonp61bPiXEUmqFRW/Yh/WwP/o7La5DYG7LYAFRb1o9o3z9ie6mTGkokkZfr/Mmh8MiIkMwdrThcJUkkUGXYIB4p2b1QQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
